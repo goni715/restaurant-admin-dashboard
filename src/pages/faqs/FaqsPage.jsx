121b88b78dd27f89c7aa0393e4c2767f7bfef57f
goni715 <halesgoni715149123@gmail.com>
2025-04-29 22:29:40 +0600

    owner completed

--- src/pages/faqs/FaqsPage.jsx
+++ src/pages/faqs/FaqsPage.jsx
@@ -4,6 +4,7 @@ import CreateFaqModal from "../../components/modal/faq/CreateFaqModal";
 
 
 const FaqsPage = () => {
+
   return (
     <div className="p-6 mx-auto h-full bg-white rounded-lg border border-gray-200">
       {/* FAQ List */}
